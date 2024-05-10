b5030b7d3c075e16cc2425c1ce58569e4803047c
Bryan Gallo <bryangallo95@hotmail.com>
2024-05-10 02:03:17 -0500

    Iniciando Proyecto Propinas-TS

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <>
+            <h1 className="text-center text-4xl font-bold bg-teal-500 p-5">Calculadora de Propinas y Consumo</h1>
+        </>
+    );
+}
+
+export default App;
